ca0f2154da4740967005dc4fc6a20e7e6946d388
Alex Roper <aroper@umich.edu>
2013-10-16 00:08:10 -0400

    Rewrite awesome to use proxy_actions.

--- grammar/awesome.py
+++ grammar/awesome.py
@@ -1,64 +1,43 @@
-from dragonfly import (Grammar, AppContext, CompoundRule, Choice, Dictation, List, Optional, Literal, Context, Repetition)
+from dragonfly import (Grammar, AppContext, CompoundRule, Choice, Dictation, List, Optional, Literal, Context, Repetition, MappingRule)
 import natlink, os
 from comsat import ComSat
 
 from raul import SelfChoice, NUMBERS
+import aenea
 
 grammar_context = AppContext(executable="notepad")
 grammar = Grammar("awesome", context=grammar_context)
 
-awesome = "Super_L"
-control = "Control_L"
-shift = "Shift_L"
-altar = "Alt_L"
-vorpal = "Hyper_L"
+from proxy_nicknames import Key, Text
+
+awesome = "w"
 
 identifiers = ["1", "one", "to", "two", "too", "3", "three", "4",
                "for", "fore", "four", "5", "five", "6", "six", "7", "seven", "8",
                "9", "nine"]
 
-class Basics(CompoundRule):
-  spec = "(whim | notion | ion) <command>"
+ion = "(whim | notion | ion) "
 
-  commands = {"screen":[awesome, control, "k"],
-              "up":[awesome, "k"],
-              "down":[awesome, "j"],
-              "left":[awesome, shift, "k"],
-              "right":[awesome, shift, "j"],
-              "change screen":[awesome, "o"],
-              "close client":[awesome, shift, "c"],
-              "snap":[awesome, control, "Return"],
-              "full":[awesome, "m"]
-              }
+class Basics(MappingRule):
+  mapping = {
+      ion + "screen":Key(awesome + "c-k"),
+      ion + "up":Key(awesome + "-k"),
+      ion + "down":Key(awesome + "-j"),
+      ion + "left":Key(awesome + "s-k"),
+      ion + "right":Key(awesome + "s-j"),
+      ion + "change screen [<n>]":Key(awesome + "-o:%(n)d"),
+      ion + "close client":Key(awesome + "s-c"),
+      ion + "snap":Key(awesome + "c-enter"),
+      ion + "full":Key(awesome + "-m"),
+      ion + "[work] <n>":Key(awesome + "-%(n)d"),
+      ion + "tag <n>":Key(awesome + "sc-%(n)d"),
+      ion + "tag marked <n>":Key(awesome + "s-%(n)d"),
+      ion + "move marked <n>":Key(awesome + "s-%(n)d")
+    }
 
-  extras = [SelfChoice("command", commands)]
-
-  def _process_recognition(self, node, extras):
-    command = self.commands[str(extras["command"])]
-    with ComSat() as connection:
-      connection.getRPCProxy().callKeyStack(" ".join(command))
-
-class Numeric(CompoundRule):
-  spec = "(whim | notion | ion) [<command>] <identifier>"
-
-  commands = {"work":[],
-              "move":[shift, control],
-              "tag":[shift, control],
-              "tag marked":[shift],
-              "move marked":[shift]}
-
-  extras = [SelfChoice("command", commands),
-            SelfChoice("identifier", identifiers)]
-
-  def _process_recognition(self, node, extras):
-    command = self.commands[str(extras.get("command", "work"))]
-    identifier = NUMBERS.get(extras["identifier"])
-    with ComSat() as connection:
-      connection.getRPCProxy().callKeyStack(" ".join([awesome] + command + [identifier]))
+  extras = [aenea.DigitalInteger("n", 1, None)]
 
 grammar.add_rule(Basics())
-grammar.add_rule(Numeric())
-
 grammar.load()
 
 def unload():
