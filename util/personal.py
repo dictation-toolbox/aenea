e16ffc1994994efe87f62cc092aa1e0ca8144f3a
Alex Roper <aroper@umich.edu>
2013-12-15 14:58:16 -0500

    Non-empty mapping in personal necessary.

--- util/personal.py
+++ util/personal.py
@@ -1,9 +1,15 @@
+# Put things you SSH to here.
 SSH_LOVED_HOSTS = {
-    }
+    "example dot com":"example.com",
+  }
 
+# Put directories commonly used here.
 FAVORITES = {
-     }
+    "projects":"~/projects",
+  }
 
+# Put places to use with autojump here.
 JUMP_PLACES = {
-     }
+    "home dir":"~",
+  }
 
