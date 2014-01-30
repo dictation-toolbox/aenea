e7d03bd60d2d5c26918c2369ea7b0a8e35094d4a
Alex Roper <alex@aroper.net>
2014-01-30 00:45:16 -0500

    Clean up the client.

--- util/aenea_client.py
+++ util/aenea_client.py
@@ -1,8 +1,8 @@
 import Tkinter as tk
-from Tkinter import *  # @UnusedWildImport
-from ttk import *  # @UnusedWildImport
 import tkFont
 import datetime
+import sys
+import ttk
 
 import communications
 import config
@@ -19,50 +19,50 @@ class AeneaClient(tk.Tk):
         self.wm_title("Aenea client - Dictation capturing")
         self.geometry('400x600+400+0')
         self.wait_visibility(self)
-        note = Notebook(self)
-        self.tab1 = Frame(note)
-        self.tab2 = Frame(note)
-        w = LabelFrame(self.tab1, text=u"Controls")
-        w.pack(side=TOP, fill=BOTH)
-        self.button1 = Button(w, text=u"Start capture",
+        note = ttk.Notebook(self)
+        self.tab1 = tk.Frame(note)
+        self.tab2 = tk.Frame(note)
+        w = tk.LabelFrame(self.tab1, text=u"Controls")
+        w.pack(side=tk.TOP, fill=tk.BOTH)
+        self.button1 = tk.Button(w, text=u"Start capture",
             command=self.start_capture)
-        self.button1.pack(side=LEFT)
-        self.button2 = Button(w, text=u"Stop capture",
-            command=self.stop_capture, state=DISABLED)
-        self.button2.pack(side=LEFT)
+        self.button1.pack(side=tk.LEFT)
+        self.button2 = tk.Button(w, text=u"Stop capture",
+            command=self.stop_capture, state=tk.DISABLED)
+        self.button2.pack(side=tk.LEFT)
 
         dFont = tkFont.Font(family="Tahoma", size=8)
 
-        l = Label(self.tab1, text=u"Capture:")
-        l.pack(side=TOP)
+        l = tk.Label(self.tab1, text=u"Capture:")
+        l.pack(side=tk.TOP)
 
-        self.tab1.text1 = Text(self.tab1, width=16, height=5, font=dFont)
-        yscrollbar = Scrollbar(self.tab1.text1, orient=VERTICAL,
+        self.tab1.text1 = tk.Text(self.tab1, width=16, height=5, font=dFont)
+        yscrollbar = tk.Scrollbar(self.tab1.text1, orient=tk.VERTICAL,
             command=self.tab1.text1.yview)
-        yscrollbar.pack(side=RIGHT, fill=Y)
+        yscrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         self.tab1.text1["yscrollcommand"] = yscrollbar.set
-        self.tab1.text1.pack(side=TOP, fill=BOTH, expand=YES)
-        self.tab1.pack(side=TOP,  fill=X)
+        self.tab1.text1.pack(side=tk.TOP, fill=tk.BOTH, expand=tk.YES)
+        self.tab1.pack(side=tk.TOP, fill=tk.X)
         self.tab1.text1.bind("<FocusIn>", lambda event: self.focus())
 
-        l = Label(self.tab1, text=u"Log:")
-        l.pack(side=TOP)
+        l = tk.Label(self.tab1, text=u"Log:")
+        l.pack(side=tk.TOP)
 
-        self.tab1.text2 = Text(self.tab1, width=16, height=5, font=dFont)
-        yscrollbar = Scrollbar(self.tab1.text2, orient=VERTICAL,
+        self.tab1.text2 = tk.Text(self.tab1, width=16, height=5, font=dFont)
+        yscrollbar = tk.Scrollbar(self.tab1.text2, orient=tk.VERTICAL,
             command=self.tab1.text2.yview)
-        yscrollbar.pack(side=RIGHT, fill=Y)
+        yscrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         self.tab1.text2["yscrollcommand"] = yscrollbar.set
-        self.tab1.text2.pack(side=TOP, fill=BOTH, expand=YES)
-        self.tab1.pack(side=TOP,  fill=X)
+        self.tab1.text2.pack(side=tk.TOP, fill=tk.BOTH, expand=tk.YES)
+        self.tab1.pack(side=tk.TOP,  fill=tk.X)
         self.tab1.text2.bind("<FocusIn>", lambda event: self.focus())
 
-        l = Label(self.tab2, text=u"Todo...")
-        l.pack(side=LEFT)
+        l = tk.Label(self.tab2, text=u"Todo...")
+        l.pack(side=tk.LEFT)
 
         note.add(self.tab1, text="Capturing")
         note.add(self.tab2, text="Configuration")
-        note.pack(side=LEFT, fill=BOTH, expand=YES)
+        note.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.YES)
 
         try:
             self.client = communications.Proxy(ip, int(port))
@@ -72,8 +72,8 @@ class AeneaClient(tk.Tk):
 
     def log(self, message):
         timeStamp = datetime.datetime.now()
-        self.tab1.text2.insert(END, "%s: %s\n" % (timeStamp, message))
-        self.tab1.text2.see(END)  # Scroll to end.
+        self.tab1.text2.insert(tk.END, "%s: %s\n" % (timeStamp, message))
+        self.tab1.text2.see(tk.END)  # Scroll to end.
 
     def start_capture(self):
         # Release VirtualBox keyboard capture.
@@ -82,21 +82,21 @@ class AeneaClient(tk.Tk):
         self.log("Starting capture")
         self.bind("<Any KeyPress>",
             lambda event: self.send_key(event.keysym))
-        self.button1.config(state=DISABLED)
-        self.button2.config(state=NORMAL)
+        self.button1.config(state=tk.DISABLED)
+        self.button2.config(state=tk.NORMAL)
 
     def stop_capture(self):
         self.log("Stopping capture")
-        self.bind("<Any KeyPress>", self.foo)
-        self.button1.config(state=NORMAL)
-        self.button2.config(state=DISABLED)
+        self.bind("<Any KeyPress>", self.dummy_event)
+        self.button1.config(state=tk.NORMAL)
+        self.button2.config(state=tk.DISABLED)
 
-    def foo(self, event):  # Dummy method.
+    def dummy_event(self, event):
         pass
 
     def send_key(self, key):
-        self.tab1.text1.insert(END, key)
-        self.tab1.text1.see(END)  # Scroll to end.
+        self.tab1.text1.insert(tk.END, key)
+        self.tab1.text1.see(tk.END)  # Scroll to end.
         if key in ('Shift_L', 'Control_L', 'Alt_L', '??'):
             return
         if key in translateKeys.keys():
