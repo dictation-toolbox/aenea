5bbc8b9f280fdd47196d4bd92ed28b5f228649a8
Alex Roper <alex@aroper.net>
2014-06-27 18:42:27 -0400

    Style cleanup.

--- server_x11.py
+++ server_x11.py
@@ -9,95 +9,133 @@ import jsonrpclib.SimpleJSONRPCServer
 
 import config
 
-_MOUSE_BUTTONS = {"left":1, "middle":2, "right":3, "wheelup":4, "wheeldown":5}
-_MOUSE_CLICKS = {"click":"click", "down":"mousedown", "up":"mouseup"}
-_KEY_PRESSES = {"press":"", "up":"up", "down":"down"}
-_MOUSE_MOVE_COMMANDS = {"absolute":"mousemove",
-                        "relative":"mousemove_relative",
-                        "relative_active":"mousemove_active"}
+_MOUSE_BUTTONS = {
+  "left": 1,
+  "middle": 2,
+  "right": 3,
+  "wheelup": 4,
+  "wheeldown": 5
+  }
+
+
+_MOUSE_CLICKS = {
+  "click": "click",
+  "down": "mousedown",
+  "up": "mouseup"
+  }
+
+
+_KEY_PRESSES = {
+  "press": "",
+  "up": "up",
+  "down": "down"
+  }
+
+
+_MOUSE_MOVE_COMMANDS = {
+  "absolute": "mousemove",
+  "relative": "mousemove_relative",
+  "relative_active": "mousemove_active"
+  }
+
 
 _SERVER_INFO = {
-    "window_manager":"awesome",
-    "operating_system":"linux",
-    "display":"X11",
-    "server":"aenea_reference",
-    "server_version":1
+  "window_manager": "awesome",
+  "operating_system": "linux",
+  "display": "X11",
+  "server": "aenea_reference",
+  "server_version": 1
   }
+
 
 _XPROP_PROPERTIES = {
-    "_NET_WM_DESKTOP(CARDINAL)":"desktop",
-    "WM_WINDOW_ROLE(STRING)":"role",
-    "_NET_WM_WINDOW_TYPE(ATOM)":"type",
-    "_NET_WM_PID(CARDINAL)":"pid",
-    "WM_LOCALE_NAME(STRING)":"locale",
-    "WM_CLIENT_MACHINE(STRING)":"client_machine",
-    "WM_NAME(STRING)":"name"
-    }
+  "_NET_WM_DESKTOP(CARDINAL)": "desktop",
+  "WM_WINDOW_ROLE(STRING)": "role",
+  "_NET_WM_WINDOW_TYPE(ATOM)": "type",
+  "_NET_WM_PID(CARDINAL)": "pid",
+  "WM_LOCALE_NAME(STRING)": "locale",
+  "WM_CLIENT_MACHINE(STRING)": "client_machine",
+  "WM_NAME(STRING)": "name"
+  }
+
 
 _MOD_TRANSLATION = {
-    "alt":"Alt_L",
-    "shift":"Shift_L",
-    "control":"Control_L",
-    "super":"Super_L",
-    "hyper":"Hyper_L",
-    "meta":"Meta_L",
-    "win":"Super_L",
-    "flag":"Super_L",
+  "alt": "Alt_L",
+  "shift": "Shift_L",
+  "control": "Control_L",
+  "super": "Super_L",
+  "hyper": "Hyper_L",
+  "meta": "Meta_L",
+  "win": "Super_L",
+  "flag": "Super_L",
   }
+
 
 _KEY_TRANSLATION = {
-    "ampersand":"ampersand",
-    "apostrophe":"apostrophe",
-    "apps":"Menu",
-    "asterisk":"asterisk",
-    "at":"at",
-    "backslash":"backslash",
-    "backspace":"BackSpace",
-    "backtick":"grave",
-    "bar":"bar",
-    "caret":"asciicircum",
-    "colon":"colon",
-    "comma":"comma",
-    "del":"Delete",
-    "dollar":"dollar",
-    "dot":"period",
-    "dquote":"quotedbl",
-    "enter":"Return",
-    "equal":"equal",
-    "exclamation":"exclam",
-    "hash":"numbersign",
-    "hyphen":"minus",
-    "langle":"less",
-    "lbrace":"braceleft",
-    "lbracket":"bracketleft",
-    "lparen":"parenleft",
-    "minus":"minus",
-    "npadd":"KP_Add",
-    "npdec":"KP_Decimal",
-    "npdiv":"KP_Divide",
-    "npmul":"KP_Multiply",
-    "percent":"percent",
-    "pgdown":"Next",
-    "pgup":"Prior",
-    "plus":"plus",
-    "question":"question",
-    "rangle":"greater",
-    "rbrace":"braceright",
-    "rbracket":"bracketright",
-    "rparen":"parenright",
-    "semicolon":"semicolon",
-    "shift":"Shift_L",
-    "slash":"slash",
-    "space":"space",
-    "squote":"apostrophe",
-    "tilde":"asciitilde",
-    "underscore":"underscore",
-    "win":"Super_L",
+  "ampersand": "ampersand",
+  "apostrophe": "apostrophe",
+  "apps": "Menu",
+  "asterisk": "asterisk",
+  "at": "at",
+  "backslash": "backslash",
+  "backspace": "BackSpace",
+  "backtick": "grave",
+  "bar": "bar",
+  "caret": "asciicircum",
+  "colon": "colon",
+  "comma": "comma",
+  "del": "Delete",
+  "dollar": "dollar",
+  "dot": "period",
+  "dquote": "quotedbl",
+  "enter": "Return",
+  "equal": "equal",
+  "exclamation": "exclam",
+  "hash": "numbersign",
+  "hyphen": "minus",
+  "langle": "less",
+  "lbrace": "braceleft",
+  "lbracket": "bracketleft",
+  "lparen": "parenleft",
+  "minus": "minus",
+  "npadd": "KP_Add",
+  "npdec": "KP_Decimal",
+  "npdiv": "KP_Divide",
+  "npmul": "KP_Multiply",
+  "percent": "percent",
+  "pgdown": "Next",
+  "pgup": "Prior",
+  "plus": "plus",
+  "question": "question",
+  "rangle": "greater",
+  "rbrace": "braceright",
+  "rbracket": "bracketright",
+  "rparen": "parenright",
+  "semicolon": "semicolon",
+  "shift": "Shift_L",
+  "slash": "slash",
+  "space": "space",
+  "squote": "apostrophe",
+  "tilde": "asciitilde",
+  "underscore": "underscore",
+  "win": "Super_L",
   }
 
+
 def update_key_translation(translation):
-  for key in (["left", "right", "up", "down", "home", "end", "tab", "insert",
-              "escape"] + ["f%i" % i for i in xrange(1, 13)]):
+  caps_keys = [
+    "left",
+    "right",
+    "up",
+    "down",
+    "home",
+    "end",
+    "tab",
+    "insert",
+    "escape"
+    ]
+  caps_keys.extend("f%i" % i for i in xrange(1, 13))
+  for key in caps_keys:
     translation[key] = key[0].upper() + key[1:]
   for index in xrange(10):
     translation["np%i" % index] = "KP_%i" % index
@@ -106,21 +144,26 @@ def update_key_translation(translation):
     translation[chr(c).upper()] = chr(c).upper()
 update_key_translation(_KEY_TRANSLATION)
 
+
 def run_command(command, executable="xdotool"):
   command_string = "%s %s" % (executable, command)
   os.system(command_string)
+
 
 def read_command(command, executable="xdotool"):
   with os.popen("%s %s" % (executable, command), "r") as fd:
     rval = fd.read()
   return rval
 
+
 def write_command(message, arguments="type --file -", executable="xdotool"):
   with os.popen("%s %s" % (executable, arguments), "w") as fd:
     fd.write(message)
 
+
 def get_active_window(_xdotool=None):
   """Returns the window id and title of the active window."""
+
   flush_xdotool(_xdotool)
   window_id = read_command("getactivewindow")
   if window_id:
@@ -130,33 +173,40 @@ def get_active_window(_xdotool=None):
   else:
     return None, None
 
+
 def get_geometry(window_id=None, _xdotool=None):
   flush_xdotool(_xdotool)
   if window_id is None:
     window_id, _ = get_active_window()
-  geo = dict([val.lower() for val in line.split("=")]
-              for line in read_command(("getwindowgeometry --shell %i"
-                                            % window_id)).strip().split("\n"))
+  geometry = read_command("getwindowgeometry --shell %i" % window_id)
+  geometry = geometry.strip().split('\n')
+  geo = dict([val.lower()
+         for val in line.split("=")]
+         for line in geometry)
   geo = dict((key, int(value)) for (key, value) in geo.iteritems())
-  return dict((key, geo[key]) for key in ("x", "y", "width", "height", "screen"))
+  relevant_keys = "x", "y", "width", "height", "screen"
+  return dict((key, geo[key]) for key in relevant_keys)
+
 
 def transform_relative_mouse_event(event):
   geo = get_geometry()
   dx, dy = map(int, map(float, event.split()))
   return [("mousemove", "%i %i" % (geo["x"] + dx, geo["y"] + dy))]
 
+
 def get_context(_xdotool=None):
-  """return a dictionary of window properties for the currently active window.
-     it is fine to include platform specific information, but at least include
-     title and executable."""
+  """return a dictionary of window properties for the currently active
+     window. it is fine to include platform specific information, but
+     at least include title and executable."""
+
   flush_xdotool(_xdotool)
   window_id, window_title = get_active_window()
   if window_id is None:
     return {}
 
   properties = {
-      "id":window_id,
-      "title":window_title,
+    "id": window_id,
+    "title": window_title,
     }
   for line in read_command("-id %s" % window_id, "xprop").split("\n"):
     split = line.split(" = ", 1)
@@ -173,9 +223,11 @@ def get_context(_xdotool=None):
   # Sigh...
   properties["executable"] = None
   try:
-    properties["executable"] = os.readlink("/proc/%s/exe" % properties["pid"])
+    proc_command = "/proc/%s/exe" % properties["pid"]
+    properties["executable"] = os.readlink(proc_command)
   except OSError:
-    ps = read_command("%s" % properties["pid"], executable="ps").split("\n")[1:]
+    ps = read_command("%s" % properties["pid"], executable="ps")
+    ps = ps.split("\n")[1:]
     if ps:
       try:
         properties["executable"] = ps[0].split()[4]
@@ -184,40 +236,59 @@ def get_context(_xdotool=None):
 
   return properties
 
-def key_press(key, modifiers=(), direction="press", count=1, count_delay=None, _xdotool=None):
-  """press a key possibly modified by modifiers. direction may be "press",
-    "down", or "up". modifiers may contain "alt", "shift", "control", "super".
-    this X11 server also supports "hyper", "meta", and "flag"
-    (same as super). count is number of times to press it. count_delay delay
-    in ms between presses."""
-  delay = "" if (count_delay is None or count < 2) else "--delay %i " % count_delay
+
+def key_press(
+    key,
+    modifiers=(),
+    direction="press",
+    count=1,
+    count_delay=None,
+    _xdotool=None
+    ):
+  """press a key possibly modified by modifiers. direction may be
+     "press", "down", or "up". modifiers may contain "alt", "shift",
+     "control", "super". this X11 server also supports "hyper",
+     "meta", and "flag" (same as super). count is number of times to
+     press it. count_delay delay in ms between presses."""
+
+  if count_delay is None or count < 2:
+    delay = ""
+  else:
+    delay = "--delay %i " % count_delay
+
   modifiers = [_MOD_TRANSLATION.get(mod, mod) for mod in modifiers]
   key_to_press = _KEY_TRANSLATION.get(key, key)
 
-  keys = (["keydown " + key for key in modifiers] +
-          (["key%s %s" % (_KEY_PRESSES[direction], key_to_press)] * count) +
-          ["keyup " + key for key in reversed(modifiers)])
+  keys = ["keydown " + k for k in modifiers]
+  keys.extend(["key%s %s" % (_KEY_PRESSES[direction], key_to_press)] * count)
+  keys.extend("keyup " + k for k in reversed(modifiers))
+
   if _xdotool is not None:
     _xdotool.extend(keys)
   else:
     run_command(delay + " ".join(keys))
 
+
 def write_text(text, paste=False, _xdotool=None):
-  """send text formatted exactly as written to active window. If paste is True,
-     will use X11 PRIMARY clipboard to paste the text instead of typing it.
-     See config.ENABLE_XSEL documentation for more information on this."""
+  """send text formatted exactly as written to active window. If paste
+     is True, will use X11 PRIMARY clipboard to paste the text instead
+     of typing it. See config.ENABLE_XSEL documentation for more
+     information on this."""
+
   # Workaround for https://github.com/jordansissel/xdotool/pull/29
   if text:
     if paste and config.ENABLE_XSEL:
-      # swap primary and secondary X11 clipboards so we can restore after paste
+      # swap primary and secondary X11 clipboards so we can
+      # restore after paste
       run_command("-x", executable="xsel")
 
       # copy the pasted text to the clipboard
       write_command(text, arguments="-i", executable="xsel")
 
       # paste by simulating midde click
-      # TODO: can we do this even in programs that don't have a middle click?
-      #       if not, we may need a blacklist of buggy programs.
+      # TODO: can we do this even in programs that don't have a
+      #     middle click?
+      #     if not, we may need a blacklist of buggy programs.
       click_mouse(2, _xdotool=_xdotool)
       flush_xdotool(_xdotool)
 
@@ -230,30 +301,53 @@ def write_text(text, paste=False, _xdotool=None):
       flush_xdotool(_xdotool)
       write_command(text, arguments="type --file - --delay 0")
 
-def click_mouse(button, direction="click", count=1, count_delay=None, _xdotool=None):
-  """click the mouse button specified. button maybe one of "right", "left",
-    "middle", "wheeldown", "wheelup". This X11 server will also accept a
-    number."""
-  delay = "" if (count_delay is None or count < 2) else "--delay %i" % count_delay
+
+def click_mouse(
+    button,
+    direction="click",
+    count=1,
+    count_delay=None,
+    _xdotool=None
+    ):
+  """click the mouse button specified. button maybe one of "right",
+     "left", "middle", "wheeldown", "wheelup". This X11 server will
+     also accept a number."""
+
+  if count_delay is None or count < 2:
+    delay = ""
+  else:
+    delay = "--delay %i " % count_delay
+
   repeat = "" if count == 1 else "--repeat %i" % count
+
   try:
     button = _MOUSE_BUTTONS[button]
   except KeyError:
     button = int(button)
 
   command = ("%s %s %s %s" %
-             (_MOUSE_CLICKS[direction], delay, repeat, button))
+         (_MOUSE_CLICKS[direction], delay, repeat, button))
 
   if _xdotool is not None:
     _xdotool.append(command)
   else:
     run_command(command)
 
-def move_mouse(x, y, reference="absolute", proportional=False, phantom=None, _xdotool=None):
-  """move the mouse to the specified coordinates. reference may be one of
-     "absolute", "relative", or "relative_active". if phantom is not None,
-     it is a button as click_mouse. If possible, click that location without
-     moving the mouse. If not, the server will move the mouse there and click."""
+
+def move_mouse(
+    x,
+    y,
+    reference="absolute",
+    proportional=False,
+    phantom=None,
+    _xdotool=None
+    ):
+  """move the mouse to the specified coordinates. reference may be one
+  of "absolute", "relative", or "relative_active". if phantom is not
+  None, it is a button as click_mouse. If possible, click that
+  location without moving the mouse. If not, the server will move the
+  mouse there and click."""
+
   geo = get_geometry()
   if proportional:
     x = geo["width"] * x
@@ -270,6 +364,7 @@ def move_mouse(x, y, reference="absolute", proportional=False, phantom=None, _xd
   else:
     run_command(" ".join(commands))
 
+
 def pause(amount, _xdotool=None):
   """pause amount in ms."""
   if _xdotool is not None:
@@ -277,32 +372,37 @@ def pause(amount, _xdotool=None):
   else:
     time.sleep(amount / 1000.)
 
+
 def server_info(_xdotool=None):
   flush_xdotool(_xdotool)
   return _SERVER_INFO
+
 
 def flush_xdotool(actions):
   if actions:
     run_command(" ".join(actions))
     del actions[:]
 
+
 def list_rpc_commands():
   _RPC_COMMANDS = {
-    "get_context":get_context,
-    "key_press":key_press,
-    "write_text":write_text,
-    "click_mouse":click_mouse,
-    "move_mouse":move_mouse,
-    "server_info":server_info,
-    "pause":pause,
-  }
+    "get_context": get_context,
+    "key_press": key_press,
+    "write_text": write_text,
+    "click_mouse": click_mouse,
+    "move_mouse": move_mouse,
+    "server_info": server_info,
+    "pause": pause,
+    }
   return _RPC_COMMANDS
 
+
 def multiple_actions(actions):
-  """execute multiple rpc commands, aborting on any error.
-     will not return anything ever. actions is an array of objects, possessing
-     "method", "params", and "optional" keys. See also JSON-RPC multicall.
-     Guaranteed to execute in specified order."""
+  """execute multiple rpc commands, aborting on any error. will not
+     return anything ever. actions is an array of objects, possessing
+     "method", "params", and "optional" keys. See also JSON-RPC
+     multicall.  Guaranteed to execute in specified order."""
+
   xdotool = []
   for (method, parameters, optional) in actions:
     commands = list_rpc_commands()
@@ -312,6 +412,7 @@ def multiple_actions(actions):
       break
   flush_xdotool(xdotool)
 
+
 def setup_server(host, port):
   server = jsonrpclib.SimpleJSONRPCServer.SimpleJSONRPCServer((host, port))
 
@@ -320,6 +421,7 @@ def setup_server(host, port):
   server.register_function(multiple_actions)
 
   return server
+
 
 if __name__ == "__main__":
   if len(sys.argv) == 2 and sys.argv[-1] == "getcontext":
@@ -336,7 +438,8 @@ if __name__ == "__main__":
         if os.fork() == 0:
           os.chdir("/")
           os.umask(0)
-          # Safe upper bound on number of fds we could possibly have opened.
+          # Safe upper bound on number of fds we could
+          # possibly have opened.
           for fd in range(64):
             try:
               os.close(fd)
--- test_server_x11.py
+++ test_server_x11.py
@@ -9,6 +9,7 @@ import server_x11
 HOST = "127.0.0.1"
 PORT = 58382
 
+
 class test_server_x11_info(unittest.TestCase):
   def get_context(self, return_value):
     comm = util.communications.Proxy(HOST, self.port)
@@ -18,15 +19,19 @@ class test_server_x11_info(unittest.TestCase):
   @mock.patch("server_x11.get_context")
   def test_get_context(self, get_context):
     self.port = PORT
-    return_value = {"test_successful":"Yep"}
+    return_value = {"test_successful": "Yep"}
     get_context.return_value = return_value
     get_context.__name__ = "get_context"
     self.server = server_x11.setup_server(HOST, self.port)
-    test_thread = threading.Thread(target=self.get_context, args=[return_value])
+    test_thread = threading.Thread(
+      target=self.get_context,
+      args=[return_value]
+      )
     test_thread.start()
     self.server.serve_forever()
     test_thread.join()
     get_context.assert_called_with()
+
 
 class test_server_x11_actions(unittest.TestCase):
   def setUp(self):
@@ -55,11 +60,11 @@ class test_server_x11_actions(unittest.TestCase):
     run_command.side_effect = lambda cmd: commands.append(cmd)
     self.run_request_thread(self.single_request_client(self.key_press))
     self.assertEqual(commands, [
-        'key a',
-        'keydown Shift_L key a keyup Shift_L', 
-        'keydown Shift_L', 
-        '--delay 100 key b key b key b', 
-        'keyup Shift_L'
+      'key a',
+      'keydown Shift_L key a keyup Shift_L',
+      'keydown Shift_L',
+      '--delay 100 key b key b key b',
+      'keyup Shift_L'
       ])
 
   def write_text(self, comm):
@@ -69,7 +74,10 @@ class test_server_x11_actions(unittest.TestCase):
   def test_write_text(self, write_command):
     self.port = PORT + 2
     self.run_request_thread(self.single_request_client(self.write_text))
-    write_command.assert_called_with("Hello world!")
+    write_command.assert_called_with(
+        "Hello world!",
+        arguments='type --file - --delay 0'
+        )
 
   def click_mouse(self, comm):
     comm.click_mouse(button="left", count=2)
@@ -82,11 +90,11 @@ class test_server_x11_actions(unittest.TestCase):
   def test_click_mouse(self, run_command):
     self.port = PORT + 3
     commands = [
-        mock.call('click  --repeat 2 1'),
-        mock.call('click  --repeat 2 4'),
-        mock.call('click   3'),
-        mock.call('click --delay 70 --repeat 5 3'),
-        mock.call('click   2')
+      mock.call('click  --repeat 2 1'),
+      mock.call('click  --repeat 2 4'),
+      mock.call('click   3'),
+      mock.call('click --delay 70  --repeat 5 3'),
+      mock.call('click   2')
       ]
     self.run_request_thread(self.single_request_client(self.click_mouse))
     self.assertEqual(commands, run_command.mock_calls)
@@ -109,14 +117,20 @@ class test_server_x11_actions(unittest.TestCase):
   @mock.patch("server_x11.run_command")
   def test_move_mouse(self, run_command, geo, wait, get_active_window):
     self.port = PORT + 4
-    geo.return_value = {"x":50, "y":50, "width":100, "height":100, "screen":0}
+    geo.return_value = {
+      "x": 50,
+      "y": 50,
+      "width": 100,
+      "height": 100,
+      "screen": 0
+      }
     get_active_window.return_value = 103, "test"
     commands = [
-        mock.call('mousemove 0.000000 0.000000'),
-        mock.call('mousemove 50.000000 50.000000'),
-        mock.call('mousemove --window 103 75.000000 45.000000'),
-        mock.call('mousemove 0.000000 0.000000 click 1 mousemove restore'),
-        mock.call('mousemove_relative 0.000000 50.000000')
+      mock.call('mousemove 0.000000 0.000000'),
+      mock.call('mousemove 50.000000 50.000000'),
+      mock.call('mousemove --window 103 75.000000 45.000000'),
+      mock.call('mousemove 0.000000 0.000000 click 1 mousemove restore'),
+      mock.call('mousemove_relative 0.000000 50.000000')
       ]
     self.run_request_thread(self.single_request_client(self.move_mouse))
     self.assertEqual(commands, run_command.mock_calls)
@@ -133,13 +147,19 @@ class test_server_x11_actions(unittest.TestCase):
   @mock.patch("server_x11.run_command")
   def test_drag_mouse(self, run_command, geo, get_active_window):
     self.port = PORT + 5
-    geo.return_value = {"x":50, "y":50, "width":100, "height":100, "screen":0}
+    geo.return_value = {
+      "x": 50,
+      "y": 50,
+      "width": 100,
+      "height": 100,
+      "screen": 0
+      }
     get_active_window.return_value = 103, "test"
     commands = [
-        mock.call('mousemove 0.000000 0.000000'),
-        mock.call('mousedown   1'),
-        mock.call('mousemove 100.000000 100.000000'),
-        mock.call('mouseup   1')
+      mock.call('mousemove 0.000000 0.000000'),
+      mock.call('mousedown   1'),
+      mock.call('mousemove 100.000000 100.000000'),
+      mock.call('mouseup   1')
       ]
     self.run_request_thread(self.single_request_client(self.drag_mouse))
     self.assertEqual(commands, run_command.mock_calls)
@@ -169,7 +189,8 @@ class test_server_x11_actions(unittest.TestCase):
     self.key_press(batch)
     self.click_mouse(batch)
     self.pause(batch)
-    util.communications.Proxy(HOST, self.port).execute_batch(batch._commands)
+    proxy = util.communications.Proxy(HOST, self.port)
+    proxy.execute_batch(batch._commands)
     self.server.shutdown()
 
   @mock.patch("server_x11.write_command")
@@ -177,11 +198,13 @@ class test_server_x11_actions(unittest.TestCase):
   @mock.patch("server_x11.run_command")
   def test_multiple_actions(self, run_command, flush, write_command):
     calls = []
+
     def mock_flush(actions):
       """Mock has issues with the del [:]."""
       if actions:
         calls.append(actions[:])
       del actions[:]
+
     flush.side_effect = mock_flush
     self.port = PORT + 7
     self.server = server_x11.setup_server(HOST, self.port)
@@ -193,27 +216,34 @@ class test_server_x11_actions(unittest.TestCase):
 
     # No easy way to test interleaving, so we rely on shape of flushes
     # to check proper happens-before.
-    self.assertEqual(write_command.mock_calls, [mock.call("Hello world!")] * 2)
+    self.assertEqual(
+      write_command.mock_calls,
+      [mock.call("Hello world!", arguments='type --file - --delay 0')] * 2
+      )
 
     step1 = [
-        'key a',
-        'keydown Shift_L',
-        'key a',
-        'keyup Shift_L',
-        'keydown Shift_L',
-        'key b',
-        'key b',
-        'key b',
-        'keyup Shift_L'
+      'key a',
+      'keydown Shift_L',
+      'key a',
+      'keyup Shift_L',
+      'keydown Shift_L',
+      'key b',
+      'key b',
+      'key b',
+      'keyup Shift_L'
       ]
 
     step2 = [
-        'click  --repeat 2 1',
-        'click  --repeat 2 4',
-        'click   3',
-        'click --delay 70 --repeat 5 3',
-        'click   2',
-        'sleep 0.500000'
+      'click  --repeat 2 1',
+      'click  --repeat 2 4',
+      'click   3',
+      'click --delay 70  --repeat 5 3',
+      'click   2',
+      'sleep 0.500000'
       ]
 
     self.assertEqual(calls, [step1, step2, step1 + step2])
+
+
+if __name__ == '__main__':
+    unittest.main()
