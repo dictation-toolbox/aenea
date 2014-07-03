f94b4c572b4884fb6d9266742341c25509b7629b
Alex Roper <alex@aroper.net>
2014-07-02 22:51:59 -0400

    Add format functions to library.

---
+++ client/aenea/format.py
@@ -0,0 +1,54 @@
+def format_snakeword(text):
+    formatted = text[0][0].upper()
+    formatted += text[0][1:]
+    formatted += ('_' if len(text) > 1 else '')
+    formatted += format_score(text[1:])
+    return formatted
+
+
+def format_score(text):
+    return '_'.join(text)
+
+
+def format_camel(text):
+    return text[0] + ''.join([word[0].upper() + word[1:] for word in text[1:]])
+
+
+def format_proper(text):
+    return ''.join(word.capitalize() for word in text)
+
+
+def format_relpath(text):
+    return '/'.join(text)
+
+
+def format_abspath(text):
+    return '/' + format_relpath(text)
+
+
+def format_scoperesolve(text):
+    return '::'.join(text)
+
+
+def format_jumble(text):
+    return ''.join(text)
+
+
+def format_dotword(text):
+    return '.'.join(text)
+
+
+def format_dashword(text):
+    return '-'.join(text)
+
+
+def format_natword(text):
+    return ' '.join(text)
+
+
+def format_broodingnarrative(text):
+    return ''
+
+
+def format_sentence(text):
+    return ' '.join([text[0].capitalize()] + text[1:])
