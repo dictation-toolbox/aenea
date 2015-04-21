4a2db42b47498e02ef0a9da91a828ccb06f91290
David J. Rosenbaum <djr7c4@gmail.com>
2015-04-21 17:59:04 -0400

    add missing copyright header for alias.py

--- client/aenea/alias.py
+++ client/aenea/alias.py
@@ -1,3 +1,20 @@
+# This file is part of Aenea
+#
+# Aenea is free software: you can redistribute it and/or modify it under
+# the terms of version 3 of the GNU Lesser General Public License as
+# published by the Free Software Foundation.
+#
+# Aenea is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with Aenea.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (2014) David J. Rosenbaum
+# David J. Rosenbaum <djr7c4@gmail.com>
+
 import copy
 import re
 from aenea.wrappers import (Alternative,
